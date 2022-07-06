fc788b6a89d7a533669f4792abe09b5eb8700e95
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-07-06 14:58:07 +0300

    Домашнее задание

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        Hogwarts[] people = {
+        Hogwarts[] gryffindor = {
                 new Hogwarts("Harry", "Potter", "Gryffindor"),
                 new Hogwarts("Hermione", "Granger", "Gryffindor"),
                 new Hogwarts("Ron ", "Weasley", "Gryffindor")};
-        for (int i = 0; i < people.length; i++) {
-            Hogwarts hogwarts = people[i];
+        for (int i = 0; i < gryffindor.length; i++) {
+            Hogwarts hogwarts = gryffindor[i];
             System.out.println("Имя " + hogwarts.getFirstName() + " Фамилия " + hogwarts.getSurName() + " факультет " + hogwarts.getFaculty());
         }
-
+        System.out.println();
+        Hogwarts[] puffendui = {
+                new Hogwarts("Zacharias", "Smith", "Puffendui"),
+                new Hogwarts("Cedric ", "Diggory", "Puffendui"),
+                new Hogwarts("Justin ", "Finch-Fletchley", "Puffendui")};
+        for (int i = 0; i < puffendui.length; i++) {
+            Hogwarts hogwarts = puffendui[i];
+            System.out.println("Имя " + hogwarts.getFirstName() + " Фамилия " + hogwarts.getSurName() + " факультет " + hogwarts.getFaculty());
+        }
     }
 }
